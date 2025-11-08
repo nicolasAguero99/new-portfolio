a4a7c206706f1e45abd0e982c61aa55ce1f14d6a
nicolasAguero99 <nico.agueroo99@gmail.com>
2025-11-08 02:10:56 -0300

    fix 404 redirect

--- src/pages/404.ts
+++
@@ -1,6 +0,0 @@
-export function GET({ request }: { request: Request }) {
-  const currentUrl = request.url;
-  const lang = new URL(currentUrl).pathname.startsWith("/en/") ? "en/" : "";
-  const url = new URL(`/${lang}`, request.url);
-  return Response.redirect(url.toString(), 302);
-}
